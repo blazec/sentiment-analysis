0ed95faa2b98b2054520513e8e2df90cf3b627b2
blazec <blaise@Blaises-MacBook-Pro.local>
2017-01-15 20:35:47 -0500

    Initialized twtt.py

--- twtt.py
+++ twtt.py
@@ -1,1 +1,12 @@
 # Run this program using python 2.7
+
+import sys
+import NLPlib
+import csv
+
+
+if __name__ == '__main__':
+
+	training_csv = sys.argv[1]
+	student_num = sys.argv[2]
+	output = sys.argv[3]
