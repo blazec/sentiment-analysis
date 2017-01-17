f730539e68cc707187a64c523a73ab5d81733fc1
blazec <blaise@Blaises-MacBook-Pro.local>
2017-01-16 22:25:06 -0500

    twtt2: Replaced html code instead of removing it

--- twtt.py
+++ twtt.py
@@ -7,32 +7,36 @@ import re
 
 def twtt1(s):
 	''' Returns a new string with all html tags and attributes removed from s. '''
-	
-	return re.sub(r"<[^>]+>", "", s)
+	return re.sub(r'<[^>]+>', '', s)
 
 def twtt2(s):
-	''' Returns a new string with html character codes replaced with ASCII 
+	''' Returns a new string with html character codes (&#32-&#127) replaced with ASCII 
 	equivalent in s. '''
+	r = re.compile('&#[3][2-9]|&#[4-9][0-9]|&#1[0-1][0-9]|&#1[2][0-7]')
 	
-	r = re.compile('&#[3][2-9]|&#[4-9][0-9]|&#1[0-1][0-9]|&#1[2][0-7]') # detects &#
-
 	while r.search(s):
-		s = r.sub('', s)
+		html_code = r.group()
+		
+		# extract ASCII number from html code
+		ascii_code = re.search('[0-9]{2,3}', html_code).group()
+
+		s = s.replace(html_code, chr(ascii_code))
 
 	return s
 
 def twtt3(s):
 	''' Removes  all URLs (tokens that begin with http or wwww) '''
-	return re.sub(r"http\S*\s|www.\S*\s", "", s)
+	return re.sub(r'http\S*\s|www.\S*\s', "", s)
 
 def twtt4(s):
-	return ""
+	''' Returns a new string where @ and # (twitter symbols) are removed.'''
+	return re.sub(r'@|#', '', s)
 
 def twtt5(s):
 	return ""
 
 def twtt6(s):
-	return ""
+	pass
 
 def twtt7(s):
 	return ""
