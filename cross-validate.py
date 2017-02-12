bb6a7f7dd36855387fb89c9fa358a7d4f7217bf7
blazec <blaise@Blaises-MacBook-Pro.local>
2017-02-12 17:05:39 -0500

    Updated cross-validate script

--- cross-validate.py
+++ cross-validate.py
@@ -1,15 +1,62 @@
 # cross-validate using files partition-1 ... partition-10
+def write_arff_file(f,data):
+	with open(filename, 'w') as f:
+		attributes = ['countfirstpersonpronouns',
+						'countsecondpersonpronouns',
+						'countthirdpersonpronouns',
+						'countcoordinatingconjunctions',
+						'countpasttenseverbs',
+						'countfuturetenseverbs',
+						'countcommas',
+						'countcolonssemicolons',
+						'countdashes',
+						'countparentheses',
+						'countellipses',
+						'countcommonnouns',
+						'countpropernouns',
+						'countadverbs',
+						'countwhwords',
+						'countslangacronyms',
+						'countuppercase',
+						'averagelengthsentences',
+						'averagelengthtokens',
+						'numbersentences']
+		f.write('% 1. Title: Tweets Sentiment Analysis\n')
+		f.write('@RELATION tweetfeatures\n\n')
 
-def concatenate_arff_files(f,output):
+		for attr in attributes:
+			f.write('@ATTRIBUTE ' + attr + ' NUMERIC\n')
+		f.write('@ATTRIBUTE class {0,4}\n\n')
+		f.write('@DATA\n')
 
-    
-    lines = [line.rstrip() for line in arff_file]
-    # Data starts at index 26
-    lines = lines[26:len(lines)]
+		for d in data:
+			f.write(d + '\n')
+
+def append_data(f, l):
+	with open(f) as arff_file:
+		lines = [line.rstrip() for line in arff_file]
+		lines = lines[26:len(lines)]
+
+	return l + lines
+
+
+if __name__ == '__main__':
+	for i in range(10):
+		filename = 'bucket-all-not-' + str(i + 1) + '.arff'
+		data = []
+		for j in range(0, i) + range(i+1,10):
+			filename_j = 'partition-' + str(j+1) + '.arff'
+			data = append_data(filename_j, data)
+		write_arff_file(filename, data)
+
+# for i in range(10):
+# 	filename = 'bucket-all-not-' + str(i+1) + '.arff'
+# 	data = []
+# 	for j in (range(0,i) + range(i+1,10)):
+#     	filename_j = 'partition-' + str(j+1) + '.arff'
+#     	data = append_data(filename_j, data)
+#     write_arff_header(filename, data)
 
 
 
-for i in range(10):
-    filename = 'partition-i' + str(i+1) + '.arff'
-    for j in range(0,i) + range(i+1,10):
-        
+			        
