6aa5a86be52adc0bb673b617da29e4da7ac6b104
blazec <blaise@Blaises-MacBook-Pro.local>
2017-02-12 17:27:14 -0500

    Updated cross validate script

--- cross-validate.py
+++ cross-validate.py
@@ -1,5 +1,5 @@
 # cross-validate using files partition-1 ... partition-10
-def write_arff_file(f,data):
+def write_arff_file(filename,data):
 	with open(filename, 'w') as f:
 		attributes = ['countfirstpersonpronouns',
 						'countsecondpersonpronouns',
@@ -32,30 +32,28 @@ def write_arff_file(f,data):
 		for d in data:
 			f.write(d + '\n')
 
-def append_data(f, l):
+def get_data(f):
 	with open(f) as arff_file:
 		lines = [line.rstrip() for line in arff_file]
 		lines = lines[26:len(lines)]
-
-	return l + lines
-
+	return lines
 
 if __name__ == '__main__':
 	for i in range(10):
-		filename = 'bucket-all-not-' + str(i + 1) + '.arff'
-		data = []
+		filename_train = 'bucket-all-not-' + str(i+1) + '.arff'
+		filename_test = 'bucket-' + str(i) + '.arff'
+		filename_i = 'partition-' + str(i+1) + '.arff'
+		data_train = []
+
 		for j in range(0, i) + range(i+1,10):
 			filename_j = 'partition-' + str(j+1) + '.arff'
-			data = append_data(filename_j, data)
-		write_arff_file(filename, data)
+			data_train = data_train + get_data(filename_j)
 
-# for i in range(10):
-# 	filename = 'bucket-all-not-' + str(i+1) + '.arff'
-# 	data = []
-# 	for j in (range(0,i) + range(i+1,10)):
-#     	filename_j = 'partition-' + str(j+1) + '.arff'
-#     	data = append_data(filename_j, data)
-#     write_arff_header(filename, data)
+		data_test = get_data(filename_i)
+
+		write_arff_file(filename_train, data_train)
+		write_arff_file(filename_test, data_test)
+
 
 
 
